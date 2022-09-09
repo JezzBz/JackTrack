ac74f07b7ef61104f72cd8d202181e641b283d6d
Gamblish <92058728+Gamblish@users.noreply.github.com>
2022-09-09 20:59:43 +0300

    +

---
+++ ClientApp/src/store/action-creators/user.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { Dispatch } from "react"
+import { MissionActionTypes, UserActionTypes } from "../../enums/actionTypes"
+import { fetchData } from "../../hooks/fetchHook"
+import { MissionAction } from "../types/mission"
+import { UserAction, UserState } from "../types/user"
+
+
+
+export const setUser = (payload: object) => {
+	return async (dispatch: Dispatch<UserAction>) => {
+		try {
+			dispatch({
+				type: UserActionTypes.FETCH_USER
+			})
+
+			const response = await fetchData('user/login', payload)
+			dispatch({
+				type: UserActionTypes.FETCH_USER_SUCCESS,
+
+				payload: response.data.user
+			})
+
+
+		} catch (e) {
+			dispatch({
+				type: UserActionTypes.FETCH_USER_ERROR,
+
+				payload: 'Неправильный логин или пароль.'
+
+			})
+		}
+	}
+}
+
